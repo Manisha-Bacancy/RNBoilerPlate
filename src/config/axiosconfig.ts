d0102ff9384fd2a2f7abe2766cbc9ec988429e3b
Manisha-Bacancy <manisha.patel@bacancy.com>
2021-07-16 15:13:02 +0530

    Implemented react native config

--- src/config/axiosconfig.ts
+++ src/config/axiosconfig.ts
@@ -2,11 +2,17 @@ import rootAxios from 'axios';
 import Qs from 'qs';
 import Config from 'react-native-config';
 
+// const Domain = {
+//   Live: 'https://jsonplaceholder.typicode.com/',
+//   Staging: 'https://staging.com/',
+// };
+// export const BASE_URL = Domain.Staging;
+export const BASE_URL = Config.BASE_URL;
+
 const createAxiosInstance = () =>
   rootAxios.create({
-    baseURL: Config.BASE_URL,
-    paramsSerializer: params =>
-      Qs.stringify(params, { arrayFormat: 'brackets' }),
+    baseURL: BASE_URL,
+    paramsSerializer: params => Qs.stringify(params, {arrayFormat: 'brackets'}),
   });
 
 export default createAxiosInstance;
